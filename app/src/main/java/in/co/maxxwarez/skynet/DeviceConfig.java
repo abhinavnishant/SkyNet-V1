87ea71507819834fef2fab71be2fc7cf684697db
abhinavnishant <abhinav.nishant@gmail.com>
2020-03-28 17:52:41 +0530

    Commit

--- app/src/main/java/in/co/maxxwarez/skynet/DeviceConfig.java
+++ app/src/main/java/in/co/maxxwarez/skynet/DeviceConfig.java
@@ -1,9 +1,13 @@
 package in.co.maxxwarez.skynet;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,7 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-public class DeviceConfig extends AppCompatActivity {
+public class DeviceConfig extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "SkyNet";
 
     @Override
@@ -47,6 +51,33 @@ public class DeviceConfig extends AppCompatActivity {
         });
 
 
+
+    }
+    @Override
+    public void onClick(View view) {
+
+        int i = view.getId();
+        if (i == R.id.addInput) {
+            Log.i(TAG, "Config: Add Input");
+            String[] colors = {"red", "green", "blue", "black"};
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Pick a color");
+            builder.setItems(colors, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // the user clicked on colors[which]
+                }
+            });
+            builder.show();
+        } else if (i == R.id.addValue) {
+            Log.i(TAG, "Config: Add Value");
+        } else if (i == R.id.addOutput) {
+            Log.i(TAG, "Config: Add Output");
+        } else if (i == R.id.addState) {
+            Log.i(TAG, "Config: Add State");
+
+        }
 
     }
 
